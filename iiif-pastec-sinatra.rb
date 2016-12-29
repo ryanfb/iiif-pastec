4c8bf145cce80f9d562f4b6f173a577aa6fdabd2
Ryan Baumann <ryan.baumann@gmail.com>
2016-12-29 16:37:37 -0500

    Return query image in results

--- iiif-pastec-sinatra.rb
+++ iiif-pastec-sinatra.rb
@@ -5,6 +5,7 @@ require 'tempfile'
 require 'json'
 require 'yaml'
 require 'yaml/store'
+require 'base64'
 
 config = YAML.load_file('config.yml')
 store = YAML::Store.new(config[:persistent_store])
@@ -20,6 +21,7 @@ post '/upload' do
   response = `curl -s -X POST --data-binary @#{params['imfile'][:tempfile].path} #{config[:pastec_server]}/index/searcher`.chomp
   $stderr.puts response
   parsed_response = JSON.parse(response)
+  @query_image = Base64.encode64(File.binread(params['imfile'][:tempfile].path))
   @images = []
   store.transaction do
     parsed_response['image_ids'].each do |image_id|
